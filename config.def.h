1a08e002305111916f2a038ba603619455870443
Ben O'Neill <ben@benoneill.xyz>
2020-03-23 15:25:49 -0400

    Use config.def.h

--- config.def.h
+++ config.def.h
@@ -1,19 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
+	/*{"",	"music",	0,	11},*/
+	/*{"",	"pacpackages",	0,	8},*/
 	/* {"",	"crypto",	0,	13}, */
-	{"",	"torrent",	20,	7},
+	/*{"",	"torrent",	20,	7},*/
 	{"",	"news",		0,	6},
 	/* {"",	"moonphase",	18000,	5}, */
-	{"",	"weather",	18000,	5},
-	{"",	"mailbox",	180,	12},
+	/*{"",	"weather",	18000,	5},*/
+	{"",	"mailbox",	300,	12},
 	{"",	"volume",	0,	10},
 	{"",	"battery",	5,	0},
-	{"",	"clock",	60,	0},
 	{"",	"internet",	5,	4},
+	{"",	"clock",	60,	0},
+	{"",    "echo \"☢$(corona US | tail -n 1 | cut -d ' ' -f 3)\"", 60, 2},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
